b5a6f66303b8a20fc92e7647724fccf5681c25c4
VERANIKA <veranika.isakova89@gmail.com>
2017-12-04 22:15:14 +0100

    Add components: Square, Board, Game

---
+++ src/components/Game.js
@@ -0,0 +1,20 @@
+import React, { PureComponent } from 'react'
+import Board from './Board';
+
+class Game extends PureComponent {
+  render() {
+    return(
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div> </div>
+          <ol> </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Game
---
+++ src/components/Square.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+class Square extends PureComponent {
+  render() {
+    return(
+      <button className="square">
+        {  }
+      </button>
+    )
+  }
+}
+
+export default Square
